61dcfcf12f824e444cefd652e08c00b5d17dc593
heyhiru <hirentimbadiya74@gmail.com>
2023-11-22 11:43:28 +0530

    implemented question model
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

---
+++ models/questionModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    subject: { type: String, required: true },
+    topic: { type: String, required: true },
+    difficulty: { type: String, required: true },
+    marks: { type: Number, required: true }
+});
+
+// Create Question model
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
