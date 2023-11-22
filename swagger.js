a97db6eff90de2bfc8f525683c081f7ca0e461d3
heyhiru <hirentimbadiya74@gmail.com>
2023-11-22 11:44:09 +0530

    added swagger.js for api documentation
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

---
+++ swagger.js
@@ -0,0 +1,76 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Question Paper Generator API',
+            version: '1.0.0',
+            description: 'API for managing questions and generating question papers',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Local development server',
+            },
+        ],
+    },
+    apis: ['./routes/questionRouter.js', './controllers/questionController.js' , './controllers/getQuestionsController.js'],
+    components: {
+        schemas: {
+            Question: {
+                type: 'object',
+                properties: {
+                    question: { type: 'string' },
+                    subject: { type: 'string' },
+                    topic: { type: 'string' },
+                    difficulty: { type: 'string' },
+                    marks: { type: 'integer' },
+                },
+                required: ['question', 'subject', 'topic', 'difficulty', 'marks'],
+                example: {
+                    question: 'Explain the concept of superconductivity',
+                    subject: 'Physics',
+                    topic: 'Superconductivity',
+                    difficulty: 'Medium',
+                    marks: 4,
+                },
+            },
+            QuestionPaper: {
+                type: 'object',
+                properties: {
+                    totalMarks: { type: 'integer' },
+                    difficultyDistribution:
+                    {
+                        typeof: 'object',
+                        properties: {
+                            easy: { type: 'integer' },
+                            medium: { type: 'integer' },
+                            hard: { type: 'integer' },
+                        }
+                    }
+                },
+                required: ['totalMarks', 'difficultyDistribution'],
+                example:
+                {
+                    "totalMarks": 100,
+                    "difficultyDistribution":
+                    {
+                        "easy": 25,
+                        "medium": 50,
+                        "hard": 25
+                    }
+                }
+                ,
+            },
+        },
+    },
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = {
+    serveSwaggerUI: swaggerUi.serve,
+    setupSwaggerUI: swaggerUi.setup(swaggerSpec),
+};
